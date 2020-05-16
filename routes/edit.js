e5a5cf38655ba6069f90a91c027cd3e4623ceda1
わかちこ。 <wakachiko0623@gmail.com>
2020-05-16 22:23:14 +0900

    画面上での編集機能を追加

--- routes/edit.js
+++ routes/edit.js
@@ -1,5 +1,44 @@
+const post = require('../lib/post');
 
-module.exports = (req, res) => {
-  res.render('chart/edit', {
+// GETリクエスト
+function Get(req, res) {
+  post.findOne({
+    where: {
+      id: req.query.id
+    }
+  }).then((post) => {
+    res.render('edit', {
+      editTarget: post
+    });
   });
+}
+
+// POSTリクエスト
+function Post(req, res) {
+  post.findOne({
+    where: {
+      id: req.query.id
+    }
+  }).then((updateData) => {
+    updateData.update({
+      task: req.body.task,
+      person: req.body.person,
+      progress: req.body.progress,
+      importance: req.body.importance,
+      taskDate: req.body.taskDate,
+      compDate: req.body.compDate,
+      manHour: req.body.manHour,
+      taskType: req.body.taskType,
+      note: req.body.note
+    });
+    console.log("updated!")
+    res.render('edit', {
+      editTarget: updateData,
+      updateMsg: "更新しました"
+    });
+  });
+}
+
+module.exports = {
+  Get, Post
 }

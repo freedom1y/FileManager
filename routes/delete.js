2422976306a0b337e62e294f4dd90f5e1ccf5bc6
わかちこ。 <wakachiko0623@gmail.com>
2020-05-21 16:22:37 +0900

    編集機能と削除機能を追加

--- routes/delete.js
+++ routes/delete.js
@@ -1,11 +1,33 @@
-function handleDelete(req, res){
-  console.log("test delete")
-  res.render('chart', {
-    xlsk: posts
+const post = require('../lib/post');
+
+function Delete(req, res){
+  post.destroy({
+    where: {
+      id: req.query.id
+    }
+  }).then((deleteData) =>{
+    console.log("delete rowid = " + req.query.id);
+    post.findAll({
+      order: [['project', 'ASC']]
+    }).then((posts) =>{
+      res.render('chart', {
+        xlsk: posts
+      });
+    });
+    
+  }).catch((err) => {
+    console.log("delete false");
+    post.findAll({
+      order: [['project', 'ASC']]
+    }).then((posts) =>{
+      res.render('chart', {
+        xlsk: posts
+      });
+    });
   });
 }
 
 
 module.exports = {
-  handleDelete
+  Delete
 }

17bc840b92457eab4ed5caf2de415007fa84188d
freedom1y <ichihara.yusuke.17@shizuoka.ac.jp>
2020-05-26 15:38:19 +0900

    typoを修正

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 const auth = require('basic-auth');
 
-
 module.exports = (req, res) => {
   console.log('[' + new Date() + '] login ' + req.connection.remoteAddress);
-  const filenames = fs.readdirSync("./uploads");
+  //const filenames = fs.readdirSync("./uploads");
   const userInfo = auth(req); 
-  res.render('index', {fileName: filenames, userName: userInfo.name});
+  //res.render('index', {fileName: filenames, userName: userInfo.name});
+  res.render('index', {userName: userInfo.name});
 }

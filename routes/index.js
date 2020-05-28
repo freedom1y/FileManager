13497cdb91127b47649888073acf79131c3cbce5
わかちこ。 <wakachiko0623@gmail.com>
2020-05-28 15:04:25 +0900

    ログイン時のみlogin ログが出力されるよう修正

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const fs = require('fs');
 const auth = require('basic-auth');
 
 module.exports = (req, res) => {
-  console.log('[' + new Date() + '] login ' + req.connection.remoteAddress);
+  if(typeof req.query.flag !== 'undefined'){
+    console.log('[' + new Date() + '] login ' + req.connection.remoteAddress);
+  }
   //const filenames = fs.readdirSync("./uploads");
   const userInfo = auth(req); 
   //res.render('index', {fileName: filenames, userName: userInfo.name});

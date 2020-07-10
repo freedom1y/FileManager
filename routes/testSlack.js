b70626818ea472cb24c25e69737465c938c179dc
わかちこ。 <wakachiko0623@gmail.com>
2020-07-10 17:07:25 +0900

    slack通知用モジュールを修正

--- routes/testSlack.js
+++ routes/testSlack.js
@@ -2,21 +2,19 @@ const toSlack = require('../lib/slackNotice');
 const auth = require('basic-auth');
 
 
+
 function Get(req, res) {
   const userInfo = auth(req);
   toSlack.notice();
-  res.render('index', {userName: userInfo.name});
+  res.render('index', { userName: userInfo.name });
 }
 
 function Post(req, res) {
+  toSlack.notice({ id: req.body.userId });
+  console.log(req.body.userId + "module done");
   const userInfo = auth(req);
-  console.log(req.body.userId);
-  toSlack.notice({id: req.body.userId});
-  res.render('index', {userName: userInfo.name});
+  res.render('index', { userName: userInfo.name });
 }
-
-// POSTリクエスト
-
 
 
 module.exports = {
